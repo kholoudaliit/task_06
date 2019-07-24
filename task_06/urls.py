1568b99f3876c153d6f317145ad408e26646d241
kholoudaliit <kholoud.ali.it@gmail.com>
2019-07-24 16:13:43 +0300

    this is task 07 ^^

--- task_06/urls.py
+++ task_06/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from restaurants import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,5 @@ urlpatterns = [
     path('restaurants/detail/<int:restaurant_id>/',views.restaurant_detail ,name='restaurant-detail'),
     path('restaurants/create/',views.restaurant_create ,name='restaurant-create'),
 ]
+
+urlpatterns += static (settings.STATIC_URL, document_root= settings.STATIC_ROOT)
